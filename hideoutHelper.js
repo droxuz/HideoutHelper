9730e566a164d6b6ffe029dbacd3a1f45c6ea736
Andrew <andrewnong54@gmail.com>
2024-09-11 13:25:50 -0400

    Setup Firebase

--- hideoutHelper.js
+++ hideoutHelper.js
@@ -1,3 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore} from 'firebase/firestore';
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBlbZhS3rKY1-fmHDkFkL_frWulpHcb9cE",
+  authDomain: "hideouthelper-a48eb.firebaseapp.com",
+  projectId: "hideouthelper-a48eb",
+  storageBucket: "hideouthelper-a48eb.appspot.com",
+  messagingSenderId: "418103930961",
+  appId: "1:418103930961:web:9001306eba6dcd858bc870",
+  measurementId: "G-LZMJQ9BHEN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//Initialize FireStore
+const db = getFirestore(app)
+
 // Get modal element
 var modal = document.getElementById("myModal");
 
