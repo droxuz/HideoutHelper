ef89e52c5c2776552b16eebed9d03c8b32073ecd
Andrew <andrewnong54@gmail.com>
2024-09-12 14:10:55 -0400

    Firebase login

--- hideoutHelper.js
+++ hideoutHelper.js
@@ -4,10 +4,11 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore} from 'firebase/firestore';
+import { getAuth, onAuthStateChanged} from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AIzaSyBlbZhS3rKY1-fmHDkFkL_frWulpHcb9cE",
   authDomain: "hideouthelper-a48eb.firebaseapp.com",
   projectId: "hideouthelper-a48eb",
   storageBucket: "hideouthelper-a48eb.appspot.com",
@@ -20,8 +21,19 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-//Initialize FireStore
-const db = getFirestore(app)
+//Initialize FireStore and Auth
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+//Auth state
+onAuthStateChanged(auth, user=>{
+    if(user != null){
+        console.log("Logged in");
+    }
+    else{
+        console.log("No user");
+    }
+});
 
 // Get modal element
 var modal = document.getElementById("myModal");
