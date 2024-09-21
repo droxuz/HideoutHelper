9f2e9e4ab0bfc2e6797d65a80ddd4525336e2531
droxuz <156976891+droxuz@users.noreply.github.com>
2024-09-21 17:15:06 -0400

    Linked Firebase and Firestore

--- hideoutHelper.js
+++ hideoutHelper.js
@@ -6,16 +6,33 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore} from 'firebase/firestore';
 import { getAuth, onAuthStateChanged} from "firebase/auth";
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
-
-// Initialize Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional\
+const firebaseConfig = {
+    apiKey: "AIzaSyDL7AlOqEol3u1LHmn7RS4i7fduyy2lmGI",
+    authDomain: "hideouthelper-7c1a6.firebaseapp.com",
+    projectId: "hideouthelper-7c1a6",
+    storageBucket: "hideouthelper-7c1a6.appspot.com",
+    messagingSenderId: "1011025758280",
+    appId: "1:1011025758280:web:4ecc9b03da3dc2f81b707d",
+    measurementId: "G-LV7BW8NQLQ"
+  };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 //Initialize FireStore and Auth
 const auth = getAuth(app);
 const db = getFirestore(app);
+
+//Writing and Reading from Firestore
+const random_data = doc(db,'Heating1/21-09-2024');
+function inserting_data(){
+    const docData = {
+        Screws: 3,
+        Bolts: 5,
+        Duct_tape: 2,
+    }
+    setDoc(random , docData);
+};
+
 
 //Auth state
 onAuthStateChanged(auth, user=>{
@@ -26,6 +43,8 @@ onAuthStateChanged(auth, user=>{
         console.log("No user");
     }
 });
+
+
 
 // Get modal element
 var modal = document.getElementById("myModal");
