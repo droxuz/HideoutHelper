c4855f8341a090c0dccc33dfdcc21908b0c40a34
droxuz <156976891+droxuz@users.noreply.github.com>
2024-09-27 23:59:37 -0400

    Fixed import bug(still many bugs) lol

--- HIDEOUTHELPER/hideoutHelperFirebase.js
+++ HIDEOUTHELPER/hideoutHelperFirebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-console.log("bugged");
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -47,37 +47,3 @@ onAuthStateChanged(auth, user=>{
 });
 
 
-// Get modal element
-var modal = document.getElementById("myModal");
-
-// Get open modal button
-var openModalBtn = document.getElementById("openModalBtn");
-
-// Get close button
-var closeBtn = document.getElementsByClassName("close")[0];
-
-// Listen for open click
-openModalBtn.addEventListener("click", function() {
-    modal.style.display = "block";
-});
-
-// Listen for close click
-closeBtn.addEventListener("click", function() {
-    modal.style.display = "none";
-});
-
-// Listen for outside click
-window.addEventListener("click", function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-});
-
-const items = ["Screws", "Bolts", "Tape", "Nuts"];
-const itemsUl = document.getElementById('itemUl');
-for (let i = 0; i<items.length; i++) {
-    let li = document.createElement('li');
-    li.className = 'item';
-    li.textContent = items[i];
-    itemsUl.appendChild(li)
-}
---
+++ HIDEOUTHELPER/hideoutHelperFunc.js
@@ -0,0 +1,34 @@
+// Get modal element
+var modal = document.getElementById("myModal");
+
+// Get open modal button
+var openModalBtn = document.getElementById("openModalBtn");
+
+// Get close button
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// Listen for open click
+openModalBtn.addEventListener("click", function() {
+    modal.style.display = "block";
+});
+
+// Listen for close click
+closeBtn.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+// Listen for outside click
+window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+const items = ["Screws", "Bolts", "Tape", "Nuts"];
+const itemsUl = document.getElementById('itemUl');
+for (let i = 0; i<items.length; i++) {
+    let li = document.createElement('li');
+    li.className = 'item';
+    li.textContent = items[i];
+    itemsUl.appendChild(li)
+}
